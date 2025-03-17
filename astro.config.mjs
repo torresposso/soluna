31c16fcd8ac2b1079cb692d65c2d772f11059b34
torresposso <erictorres.co@gmail.com>
2025-03-16 22:31:22 -0500

    add node server

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import { defineConfig } from "astro/config";
 
 import node from "@astrojs/node";
 
+import db from "@astrojs/db";
+
 export default defineConfig({
   adapter: node({
     mode: "standalone",
   }),
+
   output: "server",
+  integrations: [db()],
 });
---
+++ db/config.ts
@@ -0,0 +1,22 @@
+import { column, defineDb, defineTable } from "astro:db";
+
+const Author = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    name: column.text(),
+  },
+});
+
+const Comment = defineTable({
+  columns: {
+    authorId: column.number({ references: () => Author.columns.id }),
+    body: column.text(),
+  },
+});
+
+export default defineDb({
+  tables: {
+    Author,
+    Comment,
+  },
+});
