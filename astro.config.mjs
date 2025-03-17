0807eebee784c9d3b36f445873a8205099e390dc
torresposso <erictorres.co@gmail.com>
2025-03-16 22:11:52 -0500

    add node server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+import node from "@astrojs/node";
+
+export default defineConfig({
+  adapter: node({
+    mode: "standalone",
+  }),
+  output: "server",
+});
